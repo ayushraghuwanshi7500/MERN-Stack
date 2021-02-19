3f3662e98d9cc8614a0dd5ea3c114d13a433573b
ayushraghuwanshi7500 <66081125+ayushraghuwanshi7500@users.noreply.github.com>
2021-02-19 12:33:16 +0530

    user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,67 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator/check');
 
-
-//@route    GET api/users
-//@desc     Test Route
+const User = require('../../models/Users');
+//@route    POST api/users
+//@desc     Register User
 //@access   Public
-router.get('/', (req,res)=>res.send("user route"));
+router.post(
+  '/',
+  [
+    check('name', 'name is required').not().isEmpty(),
+    check('email', 'please include valid email address').isEmail(),
+    check(
+      'password',
+      'please enter  a password with 6 or more password'
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists.' }] });
+      }
+      // Get users gavatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      // Encrypt password using bcrypt
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      // Return jsonwebtoken
+      res.send('user registered');
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,18 +5,21 @@ const app = express();
 
 connectDB();
 
-const PORT = process.env.PORT || 5000
-app.get('/',function(req,res){
-    res.send('<h1>Connection Successful</h1>')
+// Init Middleware
+
+app.use(express.json({ extended: false }));
+
+const PORT = process.env.PORT || 5000;
+app.get('/', function (req, res) {
+  res.send('<h1>Connection Successful</h1>');
 });
 
 // Define routes
-app.use('/api/users',require('./routes/api/users'));
-app.use('/api/auth',require('./routes/api/auth'));
-app.use('/api/profile',require('./routes/api/profile'));
-app.use('/api/posts',require('./routes/api/posts'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
-
-app.listen(PORT,function(){
-    console.log(`Server started at port ${PORT}`);
-})
+app.listen(PORT, function () {
+  console.log(`Server started at port ${PORT}`);
+});
