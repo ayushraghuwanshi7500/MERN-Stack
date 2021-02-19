ef21e3479a4d850a3bbd60b95f2c6bcaa43c4bb5
ayushraghuwanshi7500 <66081125+ayushraghuwanshi7500@users.noreply.github.com>
2021-02-19 10:40:18 +0530

    MongoDB connected

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
+
+connectDB();
+
 const PORT = process.env.PORT || 5000
 app.get('/',function(req,res){
     res.send('<h1>Connection Successful</h1>')
