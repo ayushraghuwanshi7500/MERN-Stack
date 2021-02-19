7c1f5695fd1d00c3d32ca2474bfb29046f461cf2
ayushraghuwanshi7500 <66081125+ayushraghuwanshi7500@users.noreply.github.com>
2021-02-19 10:58:38 +0530

    routes files

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ app.get('/',function(req,res){
     res.send('<h1>Connection Successful</h1>')
 });
 
+// Define routes
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/profile',require('./routes/api/profile'));
+app.use('/api/posts',require('./routes/api/posts'));
+
+
 app.listen(PORT,function(){
     console.log(`Server started at port ${PORT}`);
 })
